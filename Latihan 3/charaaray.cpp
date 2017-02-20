f08351affdb7fb680e27b1ca04cc707d3bab0810
Acep A <dg.artmedia@gmail.com>
2017-02-20 03:51:37 +0000

    Pertemuan Minggu ke 3,4,5
    Pertemuan Minggu ke 3,4,5

---
+++ Latihan 3/charaaray.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "ACEPAHMAD" ;
+    char nama2[] =  "ACEPAHMAD" ;
+    char nama3[9] = {'A', 'C', 'E', 'P', 'A', 'H', 'M', 'A', 'D'};
+
+    cout << nama << endl;
+    cout << nama2 << endl;
+    cout << nama3[5] << endl; 
+
+return 0;
+}
