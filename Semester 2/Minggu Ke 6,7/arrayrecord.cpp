52a654d4b3e81ce2633be3f45663fe63f180a506
Acep A <dg.artmedia@gmail.com>
2017-05-07 19:40:40 +0700

    Array Record
    Array Record

--- Semester 2/Minggu Ke 6,7/arrayrecord.cpp
+++ Semester 2/Minggu Ke 6,7/arrayrecord.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
+// Program Array Record
+// IS Program Memasukan Data
+// FS Program Menampilkan Data
 
+// Fungsi Record / Typedef
 typedef struct {
   string nim;
   string nama;
@@ -8,13 +12,19 @@ typedef struct {
 } mahasiswa;
 
 int main (){
+    // Rumus
+    // Variable Data
     int N;
+    // Program Memasukan Data
     cout << "Masukan Jumlah Mahasiswa:"; 
     cin >> N;
-
+    
+    // Type Data mahasiswa, memiliki array mhs[N]
     mahasiswa mhs[N];
+    // Pengulangan Data
     for (int i = 0 ; i <  N ; i++)
     { 
+       // Memasukan Data Mahasiswa 
        cout << "Data Mahasiswa Ke " << i+1 << endl;
        cout << "Masukan NIM: ";
        cin >> mhs[i].nim;
@@ -24,17 +34,21 @@ int main (){
        cin >> mhs[i].ipk; 
        cout << endl;
     }
+    // Menampilkan Table
     cout << "===============================================" << endl;
     cout << "       NIM      |      NAMA     |      IPK     " << endl;
     cout << "===============================================" << endl;
-
+    
+    // Pengulangan Data
     for (int i = 0 ; i <  N ; i++)
     {
-    cout << "\t" << mhs[i].nim  << "\t|"  ;
-    cout << "\t" << mhs[i].nama << "\t|"  ;
-    cout << "\t" << mhs[i].ipk  << "\t"  ;
-    cout << endl;
+      // Menampilkan Data Mahasiswa
+      cout << "\t" << mhs[i].nim  << "\t|"  ;
+      cout << "\t" << mhs[i].nama << "\t|"  ;
+      cout << "\t" << mhs[i].ipk  << "\t"  ;
+      cout << endl;
     }
+    // Akhir Program
     return 0;
 
 }
